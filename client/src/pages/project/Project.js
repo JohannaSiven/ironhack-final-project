b439fe6dcab91b1b1b12385199729bc4613b6fbe
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-12-04 16:20:43 +0100

    set client folder

---
+++ client/src/pages/project/Project.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Project extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Project</h1>
+      </div>
+    );
+  }
+}
---
+++ client/src/pages/user/User.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class User extends Component {
+  render() {
+    return (
+      <div>
+        <h1>User</h1>
+      </div>
+    );
+  }
+}
