8c3b208ccabbf42a4f769af313bffbea2837e9f5
Johanna <siven.johanna@gmail.com>
2019-12-04 17:31:41 +0100

    projects client page setup

--- client/src/pages/project/Project.js
+++ client/src/pages/project/Project.js
@@ -1,6 +1,29 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 export default class Project extends Component {
+  state = {
+    projects: []
+  }
+
+  getProjects() {
+    axios
+    .get("/api/projects")
+    .then(response => {
+      this.setState({
+        projects: response.data
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  componentDidMount(){
+    console.log('component mounted');
+    this.getProjects();
+  }
+ 
   render() {
     return (
       <div>
