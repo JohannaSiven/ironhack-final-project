555fd92ffbecee5af347ab2cbc00cf1f3e41df46
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-12-04 14:56:25 +0100

    create user/project models

---
+++ models/Project.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    remote: {
+      type: Boolean,
+      required: true
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["Open", "On process", "Completed"]
+    },
+    requiredRoles: {
+      type: [],
+      required: true
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    contributors: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    tags: []
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const Project = mongoose.model("Project", projectSchema);
+module.exports = Project;
--- models/User.js
+++ models/User.js
@@ -1,15 +1,36 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["Frontend", "Backend", "Fullstack", "Designer", "UI/UX"]
+    },
+    skills: [],
+    portfolio: [],
+    tags: []
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
