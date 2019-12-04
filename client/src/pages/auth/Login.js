64153dcab00cd0e1bab033e55253ffc5a510ce40
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-12-04 17:40:38 +0100

    set login/signup

---
+++ client/src/pages/auth/Login.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { login } from "../../services/user";
+import { Form, Alert } from "./styles";
+
+class Login extends Component {
+  state = {
+    username: "",
+    password: "",
+    error: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    login(this.state.username, this.state.password).then(data => {
+      if (data.message) {
+        this.setState({
+          error: data.message
+        });
+      } else {
+        this.props.setUser(data);
+        this.props.history.push("/");
+      }
+    });
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </div>
+        {this.state.error && <Alert>{this.state.error}</Alert>}
+        <button type="submit">Log in</button>
+      </Form>
+    );
+  }
+}
+
+export default Login;
