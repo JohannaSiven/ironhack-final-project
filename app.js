90658f81e8cd655c212ce92f1869c387b446fc12
Johanna <siven.johanna@gmail.com>
2019-12-05 19:49:34 +0100

    heroku fix

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 hbs.registerHelper("ifUndefined", (value, options) => {
@@ -93,5 +93,10 @@ app.use("/api/projects", projectRoutes);
 
 const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
