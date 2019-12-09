f5de02c325f72f6244ad65425d6ebbf85a8ef667
Johanna <siven.johanna@gmail.com>
2019-12-09 15:12:45 +0100

    fix

---
+++ client/build/precache-manifest.73a973f908c1de3dacc78be0cd36e326.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82773c285d51c242c580b257793ebef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "921633a7177a289c63dd",
+    "url": "/static/js/2.31236e5c.chunk.js"
+  },
+  {
+    "revision": "06807711e995d73dd661",
+    "url": "/static/js/main.8e357564.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "deaaec7ede58cbce2a8b3d90038f3add",
+    "url": "/static/media/collabrains-02.deaaec7e.svg"
+  }
+]);
