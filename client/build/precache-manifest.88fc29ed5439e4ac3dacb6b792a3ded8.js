2be19925ee6b56975bbe777f856640eb4de7f8a8
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-12-11 21:36:43 +0100

    version 2.2

---
+++ client/build/precache-manifest.88fc29ed5439e4ac3dacb6b792a3ded8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9604127f921f424659ccad9a69f5d578",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3b1a0c37601362c0277",
+    "url": "/static/css/2.b8b69a64.chunk.css"
+  },
+  {
+    "revision": "c3b1a0c37601362c0277",
+    "url": "/static/js/2.1b50c40f.chunk.js"
+  },
+  {
+    "revision": "e4141f45898e7a933756",
+    "url": "/static/js/main.74416783.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "badaf319b8cec508da7e41880b308a78",
+    "url": "/static/media/collabrains-01.badaf319.svg"
+  },
+  {
+    "revision": "deaaec7ede58cbce2a8b3d90038f3add",
+    "url": "/static/media/collabrains-02.deaaec7e.svg"
+  },
+  {
+    "revision": "755f442709040c343a4131ee6c0ad614",
+    "url": "/static/media/user-02.755f4427.png"
+  },
+  {
+    "revision": "5b6affa9f4b40c666dee8de5574914c7",
+    "url": "/static/media/user-03.5b6affa9.png"
+  },
+  {
+    "revision": "237c61aa082f84948d6b4b6dc91dd319",
+    "url": "/static/media/user-04.237c61aa.png"
+  },
+  {
+    "revision": "a48164c9177aa6c9705e98da633aa1af",
+    "url": "/static/media/user-05.a48164c9.png"
+  }
+]);
